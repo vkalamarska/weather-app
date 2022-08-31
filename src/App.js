63a84ebd2b79cc45c379ad3c14c44f03db6ee6f5
Viktoriia Kalamarska <vkalamarska@gmail.com>
2022-08-31 18:22:17 +0200

    Changed background style

--- src/App.js
+++ src/App.js
@@ -5,19 +5,32 @@ const AppWrapper = styled.section`
   width: 100%;
   height: 100%;
   display: flex;
-  background-image: linear-gradient(to bottom right, #d1e6f7, #2887c1, #0061a8);
-  background-size: cover;
-  background-repeat: no-repeat;
+
+  &:before {
+    content: "";
+    width: 100%;
+    height: 100%;
+    background-image: url(${image});
+    background-size: cover;
+    position: absolute;
+    z-index: 1;
+    filter: blur(50px);
+    transform: rotate(180deg);
+    background-position: 25px 25px;
+  }
 `;
 
 const AppContainer = styled.section`
-  width: 70%;
+  width: 80%;
   height: 85%;
-  margin: 80px auto;
+  margin: 50px auto;
   background-image: url(${image});
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
+  z-index: 2;
+  border-radius: 1px;
+  box-shadow: 0px 0px 27px 0px rgba(66, 68, 90, 0.25);
 `;
 
 function App() {
