bde514a988be0644399bf19b0ad9e8db4447d9e1
vkalamarska <112348805+vkalamarska@users.noreply.github.com>
2022-08-31 18:26:26 +0200

    Add left side component (#2)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import image from "./assets/image.jpg";
+import styled from "styled-components";
+
+const AppWrapper = styled.section`
+  width: 100%;
+  height: 100%;
+  display: flex;
+
+  &:before {
+    content: "";
+    width: 100%;
+    height: 100%;
+    background-image: url(${image});
+    background-size: cover;
+    position: absolute;
+    z-index: 1;
+    filter: blur(50px);
+    transform: rotate(180deg);
+    background-position: 25px 25px;
+  }
+`;
+
+const AppContainer = styled.section`
+  width: 80%;
+  height: 85%;
+  margin: 50px auto;
+  background-image: url(${image});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  z-index: 2;
+  border-radius: 1px;
+  box-shadow: 0px 0px 27px 0px rgba(66, 68, 90, 0.25);
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+      <AppContainer></AppContainer>
+    </AppWrapper>
   );
 }
 
