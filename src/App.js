cab9149f4b58eeda06288b7870073f484bec7992
vkalamarska <112348805+vkalamarska@users.noreply.github.com>
2022-09-03 09:05:54 +0200

    Add Weather Summary component (#5)

--- src/App.js
+++ src/App.js
@@ -1,42 +1,59 @@
 import image from "./assets/image.jpg";
 import styled from "styled-components";
+import WeatherSummary from "./components/WeatherSummary";
 
 const AppWrapper = styled.section`
   width: 100%;
   height: 100%;
   display: flex;
+  overflow: hidden;
 
   &:before {
     content: "";
-    width: 100%;
-    height: 100%;
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
     background-image: url(${image});
     background-size: cover;
-    position: absolute;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
     z-index: 1;
-    filter: blur(50px);
-    transform: rotate(180deg);
-    background-position: 25px 25px;
+    filter: blur(40px);
+    transform: rotate(180deg) scale(1.1);
+    background-position: 0px 0px;
   }
 `;
 
 const AppContainer = styled.section`
-  width: 80%;
-  height: 85%;
-  margin: 50px auto;
+  width: 75%;
+  height: 90%;
+  margin: 35px auto;
   background-image: url(${image});
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
   z-index: 2;
   border-radius: 1px;
-  box-shadow: 0px 0px 27px 0px rgba(66, 68, 90, 0.25);
+  box-shadow: 0px 0px 35px 0px rgba(102, 102, 102, 0.36);
+
+  &:before {
+    content: "";
+    position: absolute;
+    inset: 0px;
+    background-image: linear-gradient(
+      180deg,
+      rgba(255, 255, 255, 0) 56%,
+      rgba(91, 91, 91, 0.46) 100%
+    );
+  }
 `;
 
 function App() {
   return (
     <AppWrapper>
-      <AppContainer></AppContainer>
+      <AppContainer>
+        <WeatherSummary></WeatherSummary>
+      </AppContainer>
     </AppWrapper>
   );
 }
