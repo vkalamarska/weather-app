7d9813f23e29d34b9371c926db9fc978045a3925
Viktoriia Kalamarska <vkalamarska@gmail.com>
2022-09-03 09:03:26 +0200

    Weather Summary

---
+++ src/components/WeatherSummary.js
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+import { WiDayCloudy } from "weather-icons-react";
+
+const SummaryContainer = styled.section`
+  display: flex;
+  position: absolute;
+  align-items: flex-end;
+  margin: 0px 0px 80px 90px;
+  top: 70%;
+  color: white;
+  text-shadow: 0px 0px 38px rgba(3, 3, 3, 1);
+`;
+
+const Temperature = styled.span`
+  font-size: 95px;
+`;
+
+const Details = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const City = styled.span`
+  font-size: 40px;
+  padding: 15px 0 3px 15px;
+`;
+
+const Time = styled.span`
+  font-size: 16px;
+  padding: 0 0 15px 15px;
+`;
+
+const WeatherCondition = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Icon = styled.span`
+  padding: 15px 0 0 15px;
+`;
+
+const Condition = styled.span`
+  font-size: 16px;
+  padding: 0 0 15px 15px;
+`;
+
+const WeatherSummary = () => {
+  return (
+    <SummaryContainer>
+      <Temperature>16°</Temperature>
+      <Details>
+        <City>London</City>
+        <Time>10:36 - Monday, 9 Sep '19</Time>
+      </Details>
+      <WeatherCondition>
+        <Icon>
+          <WiDayCloudy size={45} color="white" />
+        </Icon>
+        <Condition>Cloudy</Condition>
+      </WeatherCondition>
+    </SummaryContainer>
+  );
+};
+
+export default WeatherSummary;
